c20229d33b86f43295658bfebf805a519a11d599
noreply <noreply@yourdomain.com>
2024-08-14 13:15:50 +0800

    v0.2.0 debug

--- model/keyword_history_info/func_getlist.go
+++ model/keyword_history_info/func_getlist.go
@@ -27,25 +27,25 @@ func (u *KeyworldHistoryInfo) GetList() ([]*KeyworldHistoryInfo, int64, error) {
 	beforeHour := time.Now().Add(-24 * time.Hour).Format("2006-01-02 15:04:05")
 	switch {
 	case len(keyworld) > 0:
-		keyworldLike := "%" + keyworld + "%"
-
-		// Where("updated_at > ?", beforeHour).
-		err := model.DB.Self.Model(&KeyworldHistoryInfo{}).
-			Select("sender_username, count(*) as total").
-			Where("key_world LIKE ? AND updated_at > ?", keyworldLike, beforeHour).
-			Or("message_content_text LIKE ? AND updated_at > ?", keyworldLike, beforeHour).
-			Group("sender_username").
-			Limit(size).Offset(offset).
-			// Order("id DESC").
-			Find(&list).
-			Error
-		model.DB.Self.Model(&KeyworldHistoryInfo{}).
-			Select("sender_username").
-			Where("key_world LIKE ? AND updated_at > ?", keyworldLike, beforeHour).
-			Or("message_content_text LIKE ? AND updated_at > ?", keyworldLike, beforeHour).
-			Group("sender_username").
-			Count(&count)
-		return list, count, err
+		return u.GetListByKeyworld()
+		// keyworldLike := "%" + keyworld + "%"
+		// // Where("updated_at > ?", beforeHour).
+		// err := model.DB.Self.Model(&KeyworldHistoryInfo{}).
+		// 	Select("sender_username, count(*) as total").
+		// 	Where("key_world LIKE ? AND updated_at > ?", keyworldLike, beforeHour).
+		// 	Or("message_content_text LIKE ? AND updated_at > ?", keyworldLike, beforeHour).
+		// 	Group("sender_username").
+		// 	Limit(size).Offset(offset).
+		// 	// Order("id DESC").
+		// 	Find(&list).
+		// 	Error
+		// model.DB.Self.Model(&KeyworldHistoryInfo{}).
+		// 	Select("sender_username").
+		// 	Where("key_world LIKE ? AND updated_at > ?", keyworldLike, beforeHour).
+		// 	Or("message_content_text LIKE ? AND updated_at > ?", keyworldLike, beforeHour).
+		// 	Group("sender_username").
+		// 	Count(&count)
+		// return list, count, err
 
 	case u.SenderId != 0:
 		err := model.DB.Self.Model(&KeyworldHistoryInfo{}).
@@ -62,4 +62,32 @@ func (u *KeyworldHistoryInfo) GetList() ([]*KeyworldHistoryInfo, int64, error) {
 	default:
 		return nil, 0, fmt.Errorf("未找到符合条件的列表")
 	}
+}
+
+func (u *KeyworldHistoryInfo) GetListByKeyworld() ([]*KeyworldHistoryInfo, int64, error) {
+	var count int64
+
+	list := make([]*KeyworldHistoryInfo, 0)
+	keyworld := strings.TrimSpace(u.KeyWorld)
+	beforeHour := time.Now().Add(-24 * time.Hour).Format("2006-01-02 15:04:05")
+
+	keyworldLike := "%" + keyworld + "%"
+
+	// Where("updated_at > ?", beforeHour).
+	err := model.DB.Self.Model(&KeyworldHistoryInfo{}).
+		// Select("sender_username, count(*) as total").
+		Where("key_world LIKE ? AND updated_at > ?", keyworldLike, beforeHour).
+		// Or("message_content_text LIKE ? AND updated_at > ?", keyworldLike, beforeHour).
+		// Group("sender_username").
+		// Limit(size).Offset(offset).
+		Order("id DESC").
+		Find(&list).
+		Error
+	model.DB.Self.Model(&KeyworldHistoryInfo{}).
+		// Select("sender_username").
+		Where("key_world LIKE ? AND updated_at > ?", keyworldLike, beforeHour).
+		// Or("message_content_text LIKE ? AND updated_at > ?", keyworldLike, beforeHour).
+		// Group("sender_username").
+		Count(&count)
+	return list, count, err
 }
