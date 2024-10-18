c054b230d956530bd1d7c9d725709fac346b4590
noreply <noreply@yourdomain.com>
2024-10-18 14:36:28 +0800

    v0.2.2 debug

--- model/telebot_info/func_getall.go
+++ model/telebot_info/func_getall.go
@@ -5,23 +5,7 @@ import (
 )
 
 func GetAll() ([]TeleBotInfo, int64, error) {
-	// var err error
 	list := make([]TeleBotInfo, 0)
-	// rows, err := model.DB.Self.Model(&TeleBotInfo{}).Rows()
-	// defer rows.Close()
-	// for rows.Next() {
-	// 	var item TeleBotInfo
-	// 	if err1 := model.DB.Self.ScanRows(rows, &item); err1 != nil {
-	// 		err = err1
-	// 		continue
-	// 	} else {
-	// 		list = append(list, &item)
-	// 	}
-	// }
-	// if len(list) > 0 {
-	// 	return list, int64(len(list)), nil
-	// }
-	// return list, 0, err
 
 	if err := model.DB.Self.Model(&TeleBotInfo{}).Find(list).Error; err != nil {
 		return nil, 0, err
